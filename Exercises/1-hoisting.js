0db2814457cc39f64cb3f30976cf369d0c4a2c24
webenot <webenot@mail.ua>
2019-09-15 15:52:27 +0300

    Labs 1,2,3,4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  /* Test Errors, but works - print { a: undefined }
+  console.log({ a });
+  var a = 1;
+  */
+  const a = 1;
+  console.log({ a });
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const countTypesInArray = null;
+const countTypesInArray = arr => {
+  const typesCounts = {};
+
+  for (const el of arr) {
+    const type = typeof el;
+    typesCounts[type] = ++typesCounts[type] || 1;
+  }
+
+  return typesCounts;
+};
 
 module.exports = { countTypesInArray };
